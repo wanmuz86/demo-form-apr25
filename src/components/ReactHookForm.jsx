9ed7a91d8e0cd69e16aea169ddd37edaaeb04873
Wan Muzaffar <wanmuz86@gmail.com>
2025-04-17 11:19:57 +0800

    react hook form demo

--- src/components/ReactHookForm.jsx
+++ src/components/ReactHookForm.jsx
@@ -1,34 +1,77 @@
 import React from 'react'
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 
 const ReactHookForm = () => {
     // register: to register the input
     // handleSubmit: handleSubmission
     // errors: keep track of the errors
-    
-    const {register, handleSubmit, formState:{errors}} = useForm();
-    
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
     const onSubmit = (data) => {
         console.log(data)
 
     }
-  return (
-    <div>
-        <h2>React Hook Form</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input type="text" {...register("name")} />
-            </div>
-            <div>
-                <label htmlFor="email">Email:</label>
-                <input type="email" {...register("email")} />
-            </div>
-            <button type='submit'>Submit data</button>
-           
-        </form>
-    </div>
-  )
+    return (
+        <div>
+            <h2>React Hook Form</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label htmlFor="title">Title</label>
+                    <select {...register("title", { required: true })}>
+                        <option value="Mr">Mr</option>
+                        <option value="Mrs">Mrs</option>
+                        <option value="Miss">Miss</option>
+                        <option value="Dr">Dr</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" {...register("name", { required: true })} />
+                    {
+                        errors.name && <p style={{ color: 'red' }}>Name is required</p>
+                    }
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" {...register("email",
+                        { required: true, pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ })} />
+                    {
+                        errors.email && <p style={{ color: 'red' }}>Email is required and to be in correct format</p>
+                    }
+                </div>
+                <div>
+                    <input type="number" placeholder="Phone number" {...register("Phone number", { required: true })} />
+                </div>
+                <div>
+                    <p>Gender</p>
+                    <label htmlFor='Male'>
+                        <input {...register("gender", { required: true })} type="radio" value="male" />Male</label>
+                        <label htmlFor='Female'>
+                    <input {...register("gender", { required: true })} type="radio" value="female" />
+                    Female
+                    </label>
+                    <label htmlFor='Prefer not to state'>
+                    <input {...register("gender", { required: true })} type="radio" value="prefernottostate" />
+                    Prefer not to state
+                    </label>
+                </div>
+                <div>
+                    <p>Are you a developer?</p>
+                    <label htmlFor='yes'>
+                    <input {...register("developer", { required: true })} type="radio" value="yes" />
+                    Yes
+                    </label>
+                    <label htmlFor='no'>
+                    <input {...register("developer", { required: true })} type="radio" value="no" />
+                    No
+                    </label>
+                </div>
+                <button type='submit'>Submit data</button>
+
+            </form>
+        </div>
+    )
 }
 
 export default ReactHookForm
